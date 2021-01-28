2bd93f70b0676cb11c3745a8576ce31f1222bc0d
Lvzejin <lzj50608>
2021-01-28 18:02:50 +0800

    测试01

--- vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/DepartmentService.java
+++ vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/DepartmentService.java
@@ -18,7 +18,7 @@ import java.util.List;
  */
 @Service
 public class DepartmentService {
-    //测试
+    //测试011111
     @Autowired
     DepartmentMapper departmentMapper;
     public List<Department> getAllDepartments() {
